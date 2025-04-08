c98263f442975048581b364ba20f8371f05ee8bd
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-04-09 00:20:46 +0200

    github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid max-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-15 font-[family-name:var(--font-geist-sans)] sm:p-30">
+    <div className="grid max-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-15 font-[family-name:var(--font-geist-sans)] sm:p-15">
       <main className="row-start-2 flex flex-col items-center gap-12">
         <h1 className="items-center justify-center text-3xl">
           Welcome to Virtual Crypto Exchange
