5f7e6e21afe55c09e059a41a5b7b44d53abd75d7
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-04-09 22:53:48 +0200

    Add zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import LoginForm from "@/components/LoginForm";
-import NextImageLink from "@/components/NextImageLink";
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-    return (
-        <div className="grid max-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-15 font-[family-name:var(--font-geist-sans)] sm:p-15">
-            <main className="row-start-2 flex flex-col items-center gap-12">
-                <h1 className="text-3xl">Welcome to Virtual Crypto Exchange</h1>
-                <LoginForm />
-            </main>
-            <footer className="row-start-3 flex justify-center">
-                <NextImageLink />
-            </footer>
-        </div>
-    );
+export default async function Home() {
+    return redirect("/login")
 }
