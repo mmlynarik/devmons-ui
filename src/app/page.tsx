f97b364abbdfdeab7c0c4df33f295e7cf4f083bd
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-04-09 15:35:45 +0200

    Indent to 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,31 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid max-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-15 font-[family-name:var(--font-geist-sans)] sm:p-15">
-      <main className="row-start-2 flex flex-col items-center gap-12">
-        <h1 className="items-center justify-center text-3xl">
-          Welcome to Virtual Crypto Exchange
-        </h1>
-        <SignInForm />
-      </main>
+    return (
+        <div className="grid max-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-15 font-[family-name:var(--font-geist-sans)] sm:p-15">
+            <main className="row-start-2 flex flex-col items-center gap-12">
+                <h1 className="items-center justify-center text-3xl">
+                    Welcome to Virtual Crypto Exchange
+                </h1>
+                <SignInForm />
+            </main>
 
-      <footer className="row-start-3 flex justify-center">
-        <Link href="https://nextjs.org/" className="flex items-center gap-3">
-          Powered by
-          <Image
-            className="dark:invert"
-            src="/nextjs-icon.svg"
-            alt="Next.js logo"
-            width={32}
-            height={32}
-            priority
-          />
-        </Link>
-      </footer>
-    </div>
-  );
+            <footer className="row-start-3 flex justify-center">
+                <Link
+                    href="https://nextjs.org/"
+                    className="flex items-center gap-3"
+                >
+                    Powered by
+                    <Image
+                        className="dark:invert"
+                        src="/nextjs-icon.svg"
+                        alt="Next.js logo"
+                        width={32}
+                        height={32}
+                        priority
+                    />
+                </Link>
+            </footer>
+        </div>
+    );
 }
