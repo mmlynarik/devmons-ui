1f786f11dfc3aead8ed1d276a729a227632ac312
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-04-21 21:35:51 +0200

    Fix session logic

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -5,37 +5,32 @@ import {cookies} from "next/headers";
 const secretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(secretKey);
 
-type SessionPayload = {
+type TokenPrivateClaims = {
     userId: number;
-    aud: string;
 };
 
-export async function encrypt(payload: SessionPayload, expiresAt: Date) {
+export async function encrypt(payload: TokenPrivateClaims, expiresAt: Date, aud: string) {
     return new SignJWT(payload)
         .setProtectedHeader({alg: "HS256"})
         .setIssuedAt()
         .setExpirationTime(expiresAt)
+        .setAudience(aud)
         .sign(encodedKey);
 }
 
-export async function decrypt(session: string | undefined = "") {
-    try {
-        const {payload} = await jwtVerify(session, encodedKey, {algorithms: ["HS256"]});
-        return payload;
-    } catch (error) {
-        console.log(`Failed to verify session ${error}`);
-    }
+export async function decrypt(token: string) {
+    return (await jwtVerify(token, encodedKey, {algorithms: ["HS256"]})).payload;
 }
 
 export async function createSession(userId: number, aud: string, expires_after_secs: number) {
     const expiresAt = new Date(Date.now() + expires_after_secs * 1000);
-    const session = await encrypt({userId, aud}, expiresAt);
+    const session = await encrypt({userId}, expiresAt, aud);
     const cookieStore = await cookies();
     cookieStore.set("access_token", session, {
         httpOnly: true,
         secure: true,
         expires: expiresAt,
         sameSite: "lax",
-        path: "/dashboard",
+        path: "/",
     });
 }
---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import {NextRequest, NextResponse} from "next/server";
+import {decrypt} from "@/lib/auth/session";
+import {cookies} from "next/headers";
+
+// 1. Specify protected and public routes
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/login", "/signup", "/"];
+
+export default async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+    const isProtectedRoute = protectedRoutes.includes(path);
+    const isPublicRoute = publicRoutes.includes(path);
+
+    const access_token = (await cookies()).get("access_token")?.value;
+
+    if (!access_token) {
+        if (isProtectedRoute) {
+            return NextResponse.redirect(new URL("/login", req.nextUrl));
+        }
+    }
+    if (access_token) {
+        const payload = await decrypt(access_token)
+        if (isPublicRoute && payload.aud === "vce") {
+            return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+        }
+    }
+    return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
