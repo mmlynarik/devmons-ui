03c94c30ea4a39f63fa1731771a8d18c9e7c8bba
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-05-03 16:09:03 +0200

    middleware separation

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import {NextRequest, NextResponse} from "next/server";
 const protectedRoutes = ["/dashboard"];
 const publicRoutes = ["/login", "/signup", "/"];
 
-export default async function middleware(req: NextRequest) {
+async function authMiddleware(req: NextRequest) {
     const path = req.nextUrl.pathname;
     const isProtectedRoute = protectedRoutes.includes(path);
     const isPublicRoute = publicRoutes.includes(path);
@@ -33,6 +33,11 @@ export default async function middleware(req: NextRequest) {
         return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
     }
     return NextResponse.next();
+}
+
+export default async function middleware(req: NextRequest) {
+    const res = authMiddleware(req)
+    return res
 }
 
 // Routes on which middleware should not run
